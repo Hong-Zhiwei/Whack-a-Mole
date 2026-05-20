59b4035bc2d2b002fe759681e7292a3342fa4362
Hong-Zhiwei <113121135@gms.tcu.edu.tw>
2026-05-20 16:01:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/Whack-a-Mole/"
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
